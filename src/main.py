ac71e26ae23cd55ad54cbf2e942dc0ebf8b5054e
Swantje Plambeck <swantje.plambeck@tuhh.de>
2023-12-08 10:11:28 +0100

    Add info on operators

--- src/main.py
+++ src/main.py
@@ -25,7 +25,8 @@ def main() -> None:
                            p_crossover=0.7, p_subtree_mutation=0.1,
                            p_hoist_mutation=0.05, p_point_mutation=0.1,
                            max_samples=0.9, verbose=1,
-                           parsimony_coefficient=0.01, random_state=0)
+                           parsimony_coefficient=0.01, random_state=0,
+                           function_setiterable=('add', 'sub', 'mul', 'div'))
     est_gp.fit(X_train, y_train)
 
     print(est_gp._program)
