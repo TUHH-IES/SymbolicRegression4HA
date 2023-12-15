8729b98d714ea17449a0022589d2671fb87e6d4f
Swantje Plambeck <swantje.plambeck@tuhh.de>
2023-12-15 12:02:48 +0100

    First ecperiments with two tank and custom functionals

---
+++ src/functionals.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sys
+
+threshold_func = sys.float_info.min
+
+
+def _protected_division(x1, x2):
+    """Closure of division (x1/x2) for zero denominator."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(np.abs(x2) > threshold_func, np.divide(x1, x2), 1.)
+
+
+def _protected_sqrt(x1):
+    """Closure of square root for negative arguments."""
+    return np.sqrt(np.abs(x1))
+
+
+def _protected_inverse(x1):
+    """Closure of inverse for zero arguments."""
+    with np.errstate(divide='ignore', invalid='ignore'):
+        return np.where(np.abs(x1) > threshold_func, 1. / x1, 0.)
+
+
+def _sign(x): return np.sign(x)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,32 @@
 from gplearn.genetic import SymbolicRegressor
+from gplearn.functions import make_function
 
 import matplotlib.pyplot as plt
 import numpy as np
 import polars as pl
+from sympy import sympify, simplify
+import math
+
+import functionals
+
+converter = {
+    'sub': lambda x, y: x - y,
+    'div': lambda x, y: x / y,
+    'mydiv': lambda x, y: x / y,
+    'mul': lambda x, y: x * y,
+    'add': lambda x, y: x + y,
+    'neg': lambda x: -x,
+    'pow': lambda x, y: x ** y,
+    'sin': lambda x: math.sin(x),
+    'cos': lambda x: math.cos(x),
+    'inv': lambda x: 1 / x,
+    'inv_custom': lambda x: 1 / x,
+    'sqrt': lambda x: x ** 0.5,
+    'sqrt_custom': lambda x: x ** 0.5,
+    'pow3': lambda x: x ** 3,
+    'abs': lambda x: abs(x),
+    'sign': lambda x: -1 if x < 0 else 1
+}
 
 def main() -> None:
     # Ground truth
@@ -15,24 +39,41 @@ def main() -> None:
 
     window = 370
     # Training samples
-    X_train = data_frame[['mQp','mUb','y1','Uo','h1','mUp','mQ0','vol1','vol2']].head(window) #'my1','my',
-    y_train = data_frame['y2'].head(window)
+    X_train = data_frame[['mQp','y1','y2','Uo','h1','mQ0','mUb']].head(window) #'my1','my','vol1','vol2','mUb','mUp'= mAp saturated
+    # can I apply a normalization? -> this might not be conform with all of the operators...
+    y_train = data_frame['y1'].head(window)
     y_train = np.diff(y_train)
     y_train = np.insert(y_train, 0, y_train[0])
+    #y_train = (y_train-np.min(y_train))/(np.max(y_train)-np.min(y_train)) #normalize
+
+    #additional functions:
+    mydiv = make_function(function=functionals._protected_division,
+                        name='mydiv',
+                        arity=2)
+    sign = make_function(function=functionals._sign,
+                        name='sign',
+                        arity=1)
 
     est_gp = SymbolicRegressor(population_size=5000,
-                           generations=20, #number of generations for the symbolic regression learner
-                           stopping_criteria=0.00000001, #
+                           generations=10, #number of generations for the symbolic regression learner
+                           stopping_criteria=1e-5, #change this to improve fitness, adapt to assumed value range
                            p_crossover=0.7, p_subtree_mutation=0.1,
                            p_hoist_mutation=0.05, p_point_mutation=0.1,
                            max_samples=1.0, verbose=1,
-                           parsimony_coefficient=0, #gives the trade-off between length of the equation and the fitting value (0 = no restriction to length) 
+                           parsimony_coefficient= 1e-10, #"auto", 
+                           #parsimony_coefficient=0, #gives the trade-off between length of the equation and the fitting value (0 = no restriction to length) 
                            random_state=0,
-                           function_set=('sub', 'mul', 'div','abs','sqrt'))
-    #todo: implement sgn
+                           function_set=('sub','add','mul','abs','sqrt',mydiv,sign), #'div' might be crucial
+                           feature_names=['mQp','y1','y2','Uo','h1','mQ0','mUb']
+                           ) #'div', but that might not work good
     est_gp.fit(X_train, y_train)
+    #final fitness (option: print score on training data)
+    print(est_gp._program.raw_fitness_)
 
-    print(est_gp._program)
+    #print(est_gp._program)
+    label = f"{sympify(str(est_gp._program), locals=converter)}"
+    label = simplify(label)
+    print(label)
 
     y_gp = est_gp.predict(X_train)
     #score_gp = est_gp.score(X_test, y_test)
@@ -43,7 +84,6 @@ def main() -> None:
     ax.legend()
     plt.show()
     #print(score_gp)
-
 
 if __name__ == "__main__":
     main()
