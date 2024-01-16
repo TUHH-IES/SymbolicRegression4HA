03a3d46463dc94baa08daa6073cd7c9b74a03e16
Swantje Plambeck <swantje.plambeck@tuhh.de>
2024-01-16 14:12:52 +0100

    Try higher order derivative

--- examples/twotank/main.m
+++ examples/twotank/main.m
@@ -5,10 +5,17 @@
 
 % Run the simulink model from here
 
+%add constant values to data set
+Monit = [Monit, A1*ones(2000,1), Cvb*ones(2000,1)];
+
+%add square root of difference to data set
+Monit = [Monit, sqrt(abs(Monit(:,7)))];
+
 % Print data to csv or similar
-header = {'mQp','mUb','Uo','my1','my','h1','mUp','mQ0','y1','vol1','y2','vol2','dy'}; %{'mQp','Uo','my1','h1','mUp','mQ0','y1','vol1'};
+%header = {'mQp','mUb','Uo','my1','my','h1','mUp','mQ0','y1','vol1','y2','vol2','dy','A1','Cvb','sumsqrt'}; 
+header = {'mQp','Uo','my1','h1','mUp','mQ0','y1','vol1','dy','A1','Cvb','ysqrt'};
 %h1 seems to be reference temperature
 
-writecell([header; num2cell(Monit)],'data_twotank_withderiv.csv')
+writecell([header; num2cell(Monit)],'data_onetank_withsubst.csv')
 
 % Figure out, which columns could / should be used for model learning
