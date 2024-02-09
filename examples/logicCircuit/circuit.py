b5e4c156cd34af11a25e753f6b6ee608dac3c682
Swantje Plambeck <swantje.plambeck@tuhh.de>
2024-02-09 14:00:24 +0100

    Move examples to other repository

--- examples/logicCircuit/circuit.py
+++
@@ -1,9 +0,0 @@
-def subtractor(x: bool, y: bool, Bin: bool):
-    a = x ^ y
-    d = a ^ Bin
-    b = not x
-    c = y and b
-    e = not a
-    f = e and Bin
-    Bout = c or f
-    return [d, Bout]
