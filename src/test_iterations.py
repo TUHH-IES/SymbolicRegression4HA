7863cfd0dec5fc7376eb6389c2464a25d166e2fa
Swantje Plambeck <swantje.plambeck@tuhh.de>
2024-03-05 17:44:03 +0100

    some experiments on population example.

--- src/test_iterations.py
+++ src/test_iterations.py
@@ -14,26 +14,28 @@ def calculate(path) -> None:
     data_frame = pl.read_csv(config["file"])
 
     window = config["window"]
-    step = config["step"]
     offset = config["offset"]
+    data_frame = data_frame.slice(offset,window)
+    X_train = data_frame[config["features"]]
+    y_train = data_frame[config["target_var"]]
+
+    step = config["kwargs"]["niterations"]
     stop = config["stop"]
 
     learner = PySRRegressor(**config.get("kwargs", {}))
-    learner.warm_start = False
+    learner.warm_start = True
     results = []
-    while (offset + window < stop):
-        print(window)
-        learner.equation_file = "./test_datasetsize/" + "win_" + str(offset) + "_" + str(window) + ".csv"
-        window = min(window + step, stop-offset)
-        current_frame = data_frame.slice(offset,window)
+    iterations = config["kwargs"]["niterations"]
+    while (iterations < stop):
+        learner.equation_file = "./test_iterations/" + "it_" + str(iterations) + ".csv"
 
-        X_train = current_frame[config["features"]]
-        y_train = current_frame[config["target_var"]]
         learner.fit(X_train, y_train)
-        results.append([window,learner.sympy(),learner.get_best()["loss"],learner.get_best()["score"]])
+        results.append([iterations,learner.sympy(),learner.get_best()["loss"],learner.get_best()["score"]])
+        print(iterations,learner.get_best()["score"])
+        iterations = iterations + step
 
-    df = pd.DataFrame.from_dict(results)
-    df.to_csv("datasize_logistic.csv")
+        df = pd.DataFrame.from_dict(results)
+        df.to_csv("iterations_logistic.csv")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
