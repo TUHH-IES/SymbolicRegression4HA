4a8b8dcb71d3726d24532bc4be8708ff3c416eaa
Swantje Plambeck <swantje.plambeck@tuhh.de>
2025-02-12 15:27:25 +0100

    Clean a bit

--- src/sr4ha/core/model_extractor.py
+++ src/sr4ha/core/model_extractor.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import core.processed_data as processed_data
 
 class HybridDecisionModel:
-    def __init__(self, tree, groups):
-        self.tree = tree
-        self.groups = groups
+    def __init__(self, tree: DecisionTreeClassifier, groupedData: processed_data.GroupedData):
+        self.tree: DecisionTreeClassifier = tree
+        self.groupedData: processed_data.GroupedData = groupedData
 
 class ModelExtractor:
     def __init__(self, config):
@@ -27,6 +27,7 @@ class ModelExtractor:
             model: The model created from the grouped data.
         """
 
+        # Add current and next group id to the data
         data = pl.DataFrame()
         for group_id, group in grouped_results._groups.items():
             for window in group.windows:
@@ -34,7 +35,7 @@ class ModelExtractor:
                 group_df = group_df.with_columns(
                     pl.Series([group_id] * len(group_df)).alias("group_id"),
                     pl.Series([group_id] * len(group_df)).alias("next_id"))
-                if window[1] < len(grouped_results.data):
+                if window[1] < len(grouped_results.data): #transition points
                     window_data = grouped_results.data.slice(window[1],1).with_columns(
                             pl.Series([group_id] * 1).alias("group_id"),
                             pl.Series([grouped_results.transitions[window[1]]] * 1).alias("next_id"))
@@ -48,7 +49,7 @@ class ModelExtractor:
         print(clf.score(X, y))
         return HybridDecisionModel(clf, grouped_results)
 
-    def evaluateDecisionTreeModel(self, model, testData):
+    def evaluateDecisionTreeModel(self, model, testData, visualize: bool = True):
         """
         Evaluate the model.
 
@@ -57,26 +58,24 @@ class ModelExtractor:
             testData: The data to evaluate the model on.
         """
         #Create flow functions for all groups
-        flows = {group_id: sympy.lambdify(self.features, group.equation, "numpy") for group_id, group in model.groups._groups.items()}
+        flows = {group_id: sympy.lambdify(self.features, group.equation, "numpy") for group_id, group in model.groupedData._groups.items()}
 
         # Predict next group
         predictedModes = model.tree.predict(testData[self.features]) #TODO: correct handling of initial mode -> actually the next mode is predcited
 
-        # Use flow functions and predictedModes to predcit target value
+        # Use flow functions and predictedModes to predict target value
         prediction = pl.DataFrame().with_columns(
             pl.Series(
                 [flows[group_id](*testData[self.features][i])[0]
                  for i, group_id in enumerate(predictedModes)]
             ).alias(self.target_var))
-        
-        print(prediction[self.target_var].head())
-        print(testData[self.target_var].head())
         error = metrics.mean_squared_error(testData[self.target_var], prediction[self.target_var])
 
-        fig, ax = plt.subplots(1, 1)
-        ax.plot(testData[self.target_var])
-        ax.plot(prediction[self.target_var])
-        plt.show()
+        if visualize:
+            fig, ax = plt.subplots(1, 1)
+            ax.plot(testData[self.target_var])
+            ax.plot(prediction[self.target_var])
+            plt.show()
 
         return error
 
